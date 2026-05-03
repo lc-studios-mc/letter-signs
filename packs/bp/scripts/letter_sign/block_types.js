a9272c6383baea962a1aa916d31e5068e209b839
LuckedCoronet <lc45@lc-studios.net>
2026-05-03 11:47:09 +0900

    Move `SIGN_BLOCK_TYPE` object

--- packs/bp/scripts/letter_sign/block_types.js
+++
@@ -1,7 +0,0 @@
-export const SIGN_BLOCK_TYPE = Object.freeze({
-	NUMBER: "lettersigns:letter_sign_number",
-	ALPHABET_1: "lettersigns:letter_sign_alphabet_1",
-	ALPHABET_2: "lettersigns:letter_sign_alphabet_2",
-	SYMBOL_1: "lettersigns:letter_sign_symbol_1",
-	SYMBOL_2: "lettersigns:letter_sign_symbol_2",
-});
--- packs/bp/scripts/letter_sign/registry.js
+++ packs/bp/scripts/letter_sign/registry.js
@@ -1,4 +1,10 @@
-import { SIGN_BLOCK_TYPE } from "./block_types.js";
+const SIGN_BLOCK_TYPE = Object.freeze({
+	NUMBER: "lettersigns:letter_sign_number",
+	ALPHABET_1: "lettersigns:letter_sign_alphabet_1",
+	ALPHABET_2: "lettersigns:letter_sign_alphabet_2",
+	SYMBOL_1: "lettersigns:letter_sign_symbol_1",
+	SYMBOL_2: "lettersigns:letter_sign_symbol_2",
+});
 
 /**
  * @typedef {Object} SignDefinition
